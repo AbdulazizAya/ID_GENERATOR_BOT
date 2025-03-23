36e3586bc6c1d92086860774e9fe369c77ab908a
Abdulaziz <abdulazizayalewmussa@gmail.com>
2025-03-23 19:27:13 +0300

    Added Submit Name Command

--- Id_Creator_bot.py
+++ Id_Creator_bot.py
@@ -10,13 +10,19 @@ async def start(update:Update , context: CallbackContext):
 async def submit_photo(update:Update , context:CallbackContext):
     await update.message.reply_text("Please send me your photo for the ID.")
 
+async def Submit_name(update:Update,context:CallbackContext):
+    await update.message.reply_text("Please Type your Full Name as it should appear on the ID.")
+
+    
+
 
 def main():
     application = Application.builder().token(Bot_Token).build()
 
     application.add_handler(CommandHandler("start",start))
     application.add_handler(CommandHandler("Submit",submit_photo))
-
+    application.add_handler(CommandHandler("Submitname",Submit_name))
+    
     application.run_polling()
 
 if __name__ == '__main__':
